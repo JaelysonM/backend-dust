2866d641d99436fe2a2630c67479a600aa2a1c9a
Jaelyson Martins <jaelysonmartins@gmail.com>
2019-10-11 20:26:36 -0300

    Fix bugs

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -7,36 +7,36 @@ const tk = process.env.SECURITY_TOKEN;
 
 module.exports = {
   async index(req, res) {
-    const { token } = req.query; 
+    const { token } = req.headers; 
 
     if (token !== tk)  return  res.status(401).json({status: "Error: Invalid token"});
-
+    
     const product = await Product.find();
     
-    return res.status(200).send(JSON.parse(product));
+    return res.status(200).json(product);
   }, 
   async store(req, res) {
-    const { token } = req.query; 
+    const { token } = req.headers; 
 
-    if (token !== tk)  return  res.json({status: "Error: Invalid token"});
+    if (token !== tk)  return  res.status(401).json({status: "Error: Invalid token"});
 
     const product = await Product.create(req.body);
     
     return res.status(201).json({status: "Success, new item has been created", ...product});
   },
   async show(req, res) {
-    const { token } = req.query; 
+    const { token } = req.headers; 
 
-    if (token !== tk)  return  res.json({status: "Error: Invalid token"});
+    if (token !== tk)  return  res.status(401).json({status: "Error: Invalid token"});
 
     const product = await Product.findById(req.params.id);
 
     return res.status(product == null ?404:200 ).json(product == null?{status: "Error: 404"}:product);
   },
   async update(req, res) {
-    const { token } = req.query; 
+    const { token } = req.headers; 
 
-    if (token !== tk)  return  res.json({status: "Error: Invalid token"});
+    if (token !== tk)  return  res.status(401).json({status: "Error: Invalid token"});
 
     const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new :true} );
 
@@ -45,9 +45,9 @@ module.exports = {
     return res.status(product == null ?404:200 ).json(product == null?{status: "Error: 404"}:{status: "Success, updated and purged by socket.io"});
   },
   async destroy(req, res) {
-    const { token } = req.query; 
+    const { token } = req.headers; 
 
-    if (token !== tk)  return  res.json({status: "Error: Invalid token"});
+    if (token !== tk)  return  res.status(401).json({status: "Error: Invalid token"});
 
     const product = await Product.findByIdAndRemove(req.params.id);
 
